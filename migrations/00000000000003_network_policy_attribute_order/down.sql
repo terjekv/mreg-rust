53c85e6b4a27eb11db990cc553d4565753327d7c
Terje Kvernes <terje@kvernes.no>
2026-07-11 21:26:35 +0200

    Keep policy attributes on migration rollback

--- migrations/00000000000003_network_policy_attribute_order/down.sql
+++ migrations/00000000000003_network_policy_attribute_order/down.sql
@@ -1,8 +1,4 @@
 DROP INDEX IF EXISTS network_policies_community_template_pattern_unique;
 
-DELETE FROM network_policy_attributes
-WHERE name = 'isolated'
-  AND description = 'The network uses client isolation.';
-
 ALTER TABLE network_policy_attribute_values
     DROP COLUMN IF EXISTS position;
